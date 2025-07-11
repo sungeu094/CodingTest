1dd5b393e97360d0b1ae0149b19361d0b975c757
sungeu094 <sungeu094@gmail.com>
2025-07-11 16:52:25 +0900

    12917

---
+++ level1/level1_12917_sorting.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class level1_12917_sorting {
+    public static void main(String[] args) {
+        String s = "Zbcdefg";
+        String result = new StringBuffer(s).reverse().toString();
+
+        System.out.println(result);
+    }
+}
